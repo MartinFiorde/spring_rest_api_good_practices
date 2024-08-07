af22c4af284e35365ef1edd30cb19196df4ab0fd
fiorde90 <fiorde_90@hotmail.com>
2024-08-07 20:34:05 -0300

    security first and second iteration

--- src/main/java/com/maf/cashcard/SecurityConfig.java
+++ src/main/java/com/maf/cashcard/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,6 +18,15 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // FIRST ITERATION: all tests work except POST
+        // return http.build()
+
+        http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/cashcards/**")
+                        .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable); //IDEM: csrf -> csrf.disable()
         return http.build();
     }
 
